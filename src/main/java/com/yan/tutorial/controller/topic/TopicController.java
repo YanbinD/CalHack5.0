a0750730fd3d6fa48a573c1f02fbbf7cbf4665ce
Yanbin Deng <dyb0214@gmail.com>
2018-11-02 02:00:35 -0700

    added topicService as a service

--- src/main/java/com/yan/tutorial/controller/topic/TopicController.java
+++ src/main/java/com/yan/tutorial/controller/topic/TopicController.java
@@ -3,18 +3,19 @@ package com.yan.tutorial.controller.topic;
 import java.util.List;
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
+	
 	@RequestMapping("/topic")
 	public List<Topic> getAllTopics () {
-		return Arrays.asList(
-					new Topic ("Spring", "Spring Framework", "Spring Framework Description"), 
-					new Topic ("Java", " Core Java", "Core Java Descption"),
-					new Topic ("javascript", "JavaScript", "JavaScript Description")
-				);
+		return topicService.getAllTopics();
 	}
 }
