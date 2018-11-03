652774fd9edad6595eccf57361925758e97fb12a
Yanbin Deng <dyb0214@gmail.com>
2018-11-02 19:43:34 -0700

    added PUT/POST request

--- src/main/java/com/yan/tutorial/controller/topic/Topic.java
+++ src/main/java/com/yan/tutorial/controller/topic/Topic.java
@@ -5,7 +5,9 @@ public class Topic {
 	private String name;
 	private String description;
 	
-	
+	public Topic () {
+		
+	}
 	public Topic(String id, String name, String description) {
 		super();
 		this.id = id;
--- src/main/java/com/yan/tutorial/controller/topic/TopicController.java
+++ src/main/java/com/yan/tutorial/controller/topic/TopicController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +16,27 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 	
-	
-	@RequestMapping("/topic")
+	@RequestMapping("/topics") //default for GET request 
 	public List<Topic> getAllTopics () {
 		return topicService.getAllTopics();
+	}
+	
+	// {id} will allow /topic/xxxx to match 
+	// @PathVariable will inject xxxx to id in the parameter 
+	@RequestMapping("/topic/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	// === POST request ===
+	@RequestMapping(method=RequestMethod.POST, value="/topics") //Map this method to requests that is a POST and uri = /topics 
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	// === PUT request ===
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}") //Map this method to requests that is a POST and uri = /topics 
+	public void updatTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(topic, id);
 	}
 }
